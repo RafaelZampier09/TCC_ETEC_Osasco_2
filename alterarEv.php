c9e418009a2febee61863e164033b44cd160ef98
RafaelZampier09 <72274618+RafaelZampier09@users.noreply.github.com>
2020-10-13 19:51:30 -0300

    Add files via upload

---
+++ alterarEv.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Alterar evento </title>
+	</head>
+	
+	<body>
+		<p>logo do site</p><br><br><br>
+		<div>
+		<h1><a>logo do site</a><a>______________________________________________________________ALTERAR EVENTO
+	_______________________________________________________________</a><a>logo do site</a><br><br><br></h1>
+	    <h2>Digite as informações do evento:</h2><br>
+		<h4>Nome do evento: <input type="text" name="evento"></h4>
+		<h4>Nome do responsavel do evento: <input type="text" name="responsavel"></h4>
+		<h4>Senha do evento: <input type="password" name="senha"></h4>
+		<h4>Confirme a senha do evento: <input type="password" name="senha2"></h4>
+		<input type="submit" value="PROXIMO">
+		</div>
+		<br><br><br><p>informações do suporte</p>
+	</body>
+</html>
